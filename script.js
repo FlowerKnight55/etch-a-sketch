b620644751e977158be135ac2069627f6d9e3fc1
FlowerKnight55 <162504966+FlowerKnight55@users.noreply.github.com>
2024-08-18 01:15:19 -0400

    fix border having opacity

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function generateBoxes(numberOfBoxes){
 
     let row;
     let column;
+    let border;
     
     let size = 800/numberOfBoxes;
     let count = 0
@@ -17,13 +18,22 @@ function generateBoxes(numberOfBoxes){
         row.classList.add("row");
         row.style.height = size + "px";
         for(let j = 1; j <=numberOfBoxes; j++){
-            
+
+            border = document.createElement("nav");
+            border.classList.add("border");
+
+            border.style.width = size + "px";
+            border.style.height = size + "px";
+
             column = document.createElement("div");
-            column.classList.add("column");
             column.setAttribute("id", "box" + count);
-            column.style.width = size + "px";
-            column.style.height = size + "px";
-            row.appendChild(column);
+
+            column.style.width = size-2 + "px";
+            column.style.height = size-2 + "px";
+
+            border.appendChild(column);
+
+            row.appendChild(border);
 
             count++;
 
@@ -32,12 +42,10 @@ function generateBoxes(numberOfBoxes){
         container.appendChild(row);
     }    
 
-
 }
 
 function changeColor(event){
     let target = event.target;
-    
     const colorPallete = ["#756AB6", "#E0AED0", "#FFE5E5", "#F7EFE5", "#E2BFD9", "#C8A1E0", "#674188"];
 
     let colorSelector = Math.floor(Math.random() * 6);
@@ -45,13 +53,19 @@ function changeColor(event){
 
     let color = window.getComputedStyle(target).backgroundColor;
 
-    console.log(color);
+
 
     if(target.tagName === "DIV" && color === "rgba(0, 0, 0, 0)"){
-        target.style.backgroundColor = colorPallete[colorSelector];
-        target.style.opacity = opacityPercentage + "%";
 
-        if(opacityPercentage <= 100){
+        console.log(target);
+        
+        target.style.backgroundColor = colorPallete[colorSelector];
+
+        
+
+        target.style.opacity = opacityPercentage + "%";
+        
+        if(opacityPercentage !== 100){
             opacityPercentage += 10;
         }
     }
